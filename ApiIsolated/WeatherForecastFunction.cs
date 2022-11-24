444c834f20488eed47d42538e32c19e20a360694
Rumi4n <bociagakamil@gmail.com>
2022-11-24 09:29:15 +0100

    Stock analyze hardcoded

--- ApiIsolated/WeatherForecastFunction.cs
+++ ApiIsolated/WeatherForecastFunction.cs
@@ -36,6 +36,28 @@ namespace ApiIsolated
             return response;
         }
 
+        [Function("TornStockAnalyze")]
+        public HttpResponseData GetTornStockAnalyze([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            var randomNumber = new Random();
+
+            var result = Enumerable.Range(1, 5).Select(index => new StockRow()
+            {
+                Name = $"Stock {index}",
+                Acronym = $"ST{index}",
+                Dividend = $"Dividend {index}",
+                DividendTime = 7,
+                DividendValue = index * 1000,
+                Shares = index * 100000,
+                SharePrice = randomNumber.Next(50, 100)
+            }).ToArray();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(result);
+
+            return response;
+        }
+
         private string GetSummary(int temp)
         {
             var summary = "Mild";
---
+++ Shared/StockRow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace BlazorApp.Shared
+{
+    public class StockRow
+    {
+        public string Name{ get; set; }
+
+        public string Acronym { get; set; }
+
+        public string Dividend { get; set; }
+
+        public int DividendValue { get; set; }
+
+        public int DividendTime { get; set; }
+
+        public int Shares { get; set; }
+
+        public decimal SharePrice { get; set; }
+
+        public decimal TotalCost => Shares * SharePrice;
+
+        public decimal Roi => DividendValue / DividendTime * 365 / TotalCost * 100;
+
+        public decimal YearlyProfit => DividendValue / DividendTime * 365;
+    }
+}
