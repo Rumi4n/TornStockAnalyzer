c50f69add2e58999d722fd1e37b5c3a572321f4a
Rumi4n <bociagakamil@gmail.com>
2022-12-23 15:57:24 +0100

    Test with calue change notifier

---
+++ Shared/VariableNotifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class VariableNotifier
+    {
+        public string ApiKey { get; set; }
+
+        public VariableNotifier()
+        {
+
+        }
+
+        public async Task UpdateApiValue(string value)
+        {
+            ApiKey = value;
+
+            await Notify?.Invoke();
+
+        }
+
+        public event Func<Task> Notify;
+    }
+}
